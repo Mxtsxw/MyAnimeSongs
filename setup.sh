4ae6ae450632f059e151437dd0ed81d99c5b2a0f
Baptiste <blambert1802@gmail.com>
2021-12-15 18:43:24 +0100

    setup.sh fonctionnel

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@ read -p "voulez vous (re)créer le virtualenv ?[y/n]" reponse
 if echo "$reponse" | grep -iq "^y" ;then
     rm -rf venv
     virtualenv -p python3 venv
+    echo "Environnement créé"
 fi
 
-source $HOME/myanimesongs/venv/bin/activate
+source venv/bin/activate
 pip install -r requirements.txt
 export FLASK_APP=./app.py
 export FLASK_DEBUG=1
+
+echo -e "-------------------------------------\nsource venv/bin/activate"
