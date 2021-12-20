3292348d1b2b59dcd27225e703308a7c0ac8ddf7
Baptiste1802 <Blambert1802@gmail.com>
2021-12-21 00:37:46 +0100

    ajout config pour utiliser sqlite

--- website/app.py
+++ website/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy 
+import os.path
 
 app = Flask(__name__)
 Bootstrap(app)
+
+def mkpath(path):
+    return os.path.normpath(
+        os.path.join(os.path.dirname(__file__), path)
+    )
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'sqlite:///'+mkpath('database/songs.db')
+)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
