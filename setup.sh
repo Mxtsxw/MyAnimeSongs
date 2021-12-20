a3d6123f8e7b7ada4976811622611fb1933b6d67
Baptiste1802 <Blambert1802@gmail.com>
2021-12-20 21:24:41 +0100

    modif var FLASK_APP

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@ read -p "voulez vous (re)créer le virtualenv ?[y/n]" reponse
 if echo "$reponse" | grep -iq "^y" ;then
     rm -rf venv
     virtualenv -p python3 venv
+    source venv/bin/activate
+    pip install -r requirements.txt
     echo "Environnement créé"
 fi
 
-source venv/bin/activate
-pip install -r requirements.txt
-export FLASK_APP=./app.py
-export FLASK_DEBUG=1
+export FLASK_APP=website
 
 echo -e "-------------------------------------\nsource venv/bin/activate"
