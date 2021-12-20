bb13c7b7602042c7c843f27a65db24f20ae4c759
Baptiste1802 <Blambert1802@gmail.com>
2021-12-21 00:49:40 +0100

    creation des tables

---
+++ website/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import backref
+from .app import db
+
+class Anime(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200))
+    img = db.Column(db.String(200))
+    
+    def __repr__(self):
+        return "<Anime (%d) %s>" % (self.id, self.name)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200))
+    relation = db.Column(db.String(10))
+    interpreter = db.Column(db.String(200))
+    ytb_url = db.Column(db.Sring(200))
+    spoty_url = db.Column(db.String(200))
+    anime_id = db.Column(db.Integer, db.ForeignKey("anime.id"))
+    anime = db.relationship("Anime", backref = db.backref("songs", lazy = "dynamic"))
+
+    def __repr__(self):
+        return "<Song (%d) %s>" % (self.id, self.title)
+    
