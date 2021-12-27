dee836b84ac22f37c22d60b718464660078ccffb
Baptiste1802 <Blambert1802@gmail.com>
2021-12-27 19:55:19 +0100

    creation de la table user

--- website/models.py
+++ website/models.py
@@ -37,3 +37,11 @@ def get_songs():
 def get_song(id):
     return Song.query.get_or_404(id)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    
+    def __repr__(self):
+        return "<User (%d) %s %s>" % (self.id, self.username, self.email)
