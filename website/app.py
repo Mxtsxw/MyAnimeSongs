ec9f6912d57670e5069ba36459e4702516d94ee3
Baptiste1802 <Blambert1802@gmail.com>
2021-12-27 20:10:08 +0100

    ajout login_manager

--- website/app.py
+++ website/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap5 import Bootstrap
 from flask_sqlalchemy import SQLAlchemy 
 import os.path
+from flask_login import LoginManager, login_manager
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -20,3 +21,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = '328396918c21bf73e52e9b867c7a2dcb88a083e200022e7380bc280e3017a617'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
