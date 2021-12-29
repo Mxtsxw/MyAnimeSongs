b7700810bd752aabe6beebfcff0b0c13474d426d
Baptiste1802 <Blambert1802@gmail.com>
2021-12-29 01:03:30 +0100

    ajout route et formulaire request/song

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from .app import app, login_manager
 from flask import render_template, url_for, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, InputRequired, Length, Email
+from wtforms.validators import DataRequired, InputRequired, Length, Email, URL
 from website.models import get_anime, get_animes, get_song, get_songs, get_songs_anime, get_role_id, get_user, get_user_by_username, get_user_by_email, create_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
@@ -111,5 +111,32 @@ def home():
         "index.html",
         user = current_user,
         title = "My Anime Songs",
+        animes = get_animes()
+    )
+    
+class RequestSongForm(FlaskForm):
+    anime = StringField("Selectionnez un animé", validators = [InputRequired()], render_kw = {"list": "datalistAnime"})
+    title = StringField("Titre", validators = [InputRequired(message = "champ obligatoire"), Length(max = 60)],
+                        render_kw = {"placeholder": "Fly High!!"})
+    interpreter = StringField("Interprète", validators = [InputRequired(message = "champ obligatoire"), Length(max = 60)],
+                              render_kw = {"placeholder": "BURNOUT SYNDROMES"})
+    relation = StringField("Relation", validators = [InputRequired(message = "champ obligatoire"), Length(max = 5)],
+                           render_kw = {"placeholder": "OP2"})
+    ytb_url = StringField("URL Youtube", validators = [InputRequired(message = "champ obligatoire"), Length(max = 120)],
+                          render_kw = {"placeholder": "watch?v=txgg-fbVjf4&ab_channel=BURNOUTSYNDROMESVEVO"})
+    spoty_url = StringField("URL Spotify", validators = [InputRequired(message = "champ obligatoire"), Length(max = 120)],
+                             render_kw = {"placeholder": "3YOZLPRiTuYgItSGO41gPT?si=4e9a74511f334ef2"})
+    submit = SubmitField("Envoyer la demande")
+        
+@app.route("/request/song", methods=['GET', 'POST'])
+@login_required
+def request_song():
+    
+    form = RequestSongForm()
+    
+    return render_template(
+        "request-song.html",
+        user = current_user,
+        form = form,
         animes = get_animes()
     )
