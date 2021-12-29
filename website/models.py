308358e523713a8977bbdb8c6b193b8438a3e679
Baptiste1802 <Blambert1802@gmail.com>
2021-12-29 19:52:27 +0100

    ajout de la classe SongRequest et de la fonction de creation et getter

--- website/models.py
+++ website/models.py
@@ -42,7 +42,6 @@ def get_song(id):
 def get_anime_by_name(name):
     return Anime.query.filter_by(name = name).first()
     
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(15), unique = True)
@@ -81,3 +80,37 @@ def create_user(username, email, password, role_id):
     db.session.add(new_user)
     db.session.commit()
     return new_user
+
+class SongRequest(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200))
+    relation = db.Column(db.String(10))
+    interpreter = db.Column(db.String(200))
+    ytb_url = db.Column(db.String(200))
+    spoty_url = db.Column(db.String(200))
+    anime_id = db.Column(db.Integer, db.ForeignKey("anime.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User", backref = db.backref("song_requests", lazy = "dynamic"))
+
+    def __repr__(self):
+        return "<Song Request (%d) %s %s>" % (self.id, self.title, self.user)
+    
+def create_song_request(title, interpreter, relation, ytb_url, spoty_url, anime_name, user_id):
+    anime_id = get_anime_by_name(anime_name).id
+    obj = SongRequest(
+        title = title,
+        relation = relation,
+        interpreter = interpreter,
+        ytb_url = ytb_url,
+        spoty_url = spoty_url,
+        anime_id = anime_id,
+        user_id = user_id
+    )
+    db.session.add(obj)
+    db.session.commit()
+    
+def get_song_requests(username):
+    return User.query.filter_by(username = username).song_requests.all()
+
+def get_song_request(id):
+    return SongRequest.query.get(id)
