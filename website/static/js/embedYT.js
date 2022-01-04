54f08e293a508eb3817e759856d8e710de96750e
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2022-01-04 20:27:30 +0100

    Mofication de l'embed YT

--- website/static/js/embedYT.js
+++ website/static/js/embedYT.js
@@ -1,10 +1,11 @@
 console.log("Youtube Embed script has been loaded")
 
-function on() {
-    document.getElementById("overlay").innerHTML = '<iframe id="ytplayer" width="560" height="315" src="https://www.youtube.com/embed/qAYWw67yiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+function on(url) {
+    document.getElementById("overlay").innerHTML = `<iframe id="ytplayer" type="text/html" width="960" height="540" src="http://www.youtube.com/embed/${url}" loading="lazy"></iframe>`
     document.getElementById("overlay").style.display = "block";
   }
   
   function off() {
+    document.getElementById("overlay").innerHTML = ""
     document.getElementById("overlay").style.display = "none";
   } 
