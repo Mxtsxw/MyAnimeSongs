1847788942ffe2c07949393d19dd2ee6258a248d
Matthieu RANDRIANTSOA <randriantsoamatthieu@gmail.com>
2022-01-05 17:36:06 +0100

    Moficiation paramètre pour l'embed Youtube

--- website/static/js/embedYT.js
+++ website/static/js/embedYT.js
@@ -1,7 +1,7 @@
 console.log("Youtube Embed script has been loaded")
 
 function on(url) {
-    document.getElementById("overlay").innerHTML = `<iframe id="ytplayer" type="text/html" width="960" height="540" src="http://www.youtube.com/embed/${url}" loading="lazy"></iframe>`
+    document.getElementById("overlay").innerHTML = `<iframe id="ytplayer" type="text/html" src="http://www.youtube.com/embed/${url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`
     document.getElementById("overlay").style.display = "block";
   }
   
