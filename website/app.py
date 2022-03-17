50ffdbcde08423e0d51782d29eb2b4bd8ca9749a
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2022-03-18 00:07:55 +0100

    Configuration module pour la mise en place de l'API

--- website/app.py
+++ website/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_bootstrap5 import Bootstrap
 from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow
 import os.path
 from flask_login import LoginManager, login_manager
+from flask_cors import CORS
 
 app = Flask(__name__)
 Bootstrap(app)
+CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 def mkpath(path):
     return os.path.normpath(
@@ -18,7 +22,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+# Init db
 db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
 
 app.config['SECRET_KEY'] = '328396918c21bf73e52e9b867c7a2dcb88a083e200022e7380bc280e3017a617'
 
